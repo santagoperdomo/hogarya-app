7b92f75dedbd27e39a49ec539e1a70c86d07e2b0
HogarYa Developer <hogarya@example.com>
2026-05-14 10:36:01 -0500

    Correcion base de datos2

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import helmet from 'helmet';
+import mongoose from 'mongoose';
 import rateLimit from 'express-rate-limit';
 import path from 'path';
 import { fileURLToPath } from 'url';
